ec5e67682db011e7b76e5f0585427dc4a3745a84
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-12-09 22:53:46 +0100

    update photogallery component

--- src/components/asset/Nomatch.js
+++ src/components/asset/Nomatch.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Error404 from './images/SeekPng.com_404-png_2123432.png'
 
 const Nomatch = () => {
   return (
     <div className='w-screen'>
-        <div className="flex justify-between mx-auto w-11/12">
+        <div className="block m-0 md:flex justify-between mx-auto w-11/12">
             <div className='my-auto'>
                 <h1 className='text-9xl mb-10 font-bold'>Oops!</h1>
                 <h3>We can't seem to find the page you're looking for</h3>
                 <h4>Error code: 404</h4>
                 <div className="text-red-900">
-                    <p>Home</p>
-                    <p>Travels</p>
+                   <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">About</Link></li>
+                    <li><Link to="/">Travels</Link></li>
+                   </ul>
                 </div>
             </div>
-            <div>
+            <div className=''>
                 <img src={Error404} alt="" className='w-fit object-cover h-fit h-screen w-screen'/>
             </div>
         </div>
--- src/components/homefiles/PhotoGallery.js
+++ src/components/homefiles/PhotoGallery.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import '../asset/styles/photogallery.css'
+import galleryImg1 from '../asset/images/beach1.png'
 
 const PhotoGallery = () => {
   return (
     <div>
         <div className="slope1">
-
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        {/* <img src={galleryImg1} alt="" loading='lazy' /> */}
         </div>
         <div className="slope">
             <h5>Gallery</h5>
             <h4>Photos from Our Tours</h4>
         </div>
         <div className="slope2">
-
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
+        <img src={galleryImg1} alt="" loading='lazy' />
         </div>
     </div>
   )
