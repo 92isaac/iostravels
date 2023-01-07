7975cec3e9ee0e09d1a55df8c7111e07725fdc8b
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-01-07 14:55:38 +0100

    about.js

--- src/components/routes/About.js
+++ src/components/routes/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Abouthero from '../aboutfiles/Abouthero'
+import Aboutmore from '../aboutfiles/Aboutmore'
+import Aboutmore2 from '../aboutfiles/Aboutmore2'
 
 const About = () => {
   return (
     <>
     <Abouthero />
+    <Aboutmore />
+    <Aboutmore2 />
     </>
   )
 }
